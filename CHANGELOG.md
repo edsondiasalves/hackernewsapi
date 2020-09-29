84295462b977fd5418a824af513aa6936cb0a2ec
Edson Moisinho <ctw00501@MacBook-Pro-de-Administrator-3.local>
2020-09-29 21:00:10 +0100

    first commit

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# 0.0.1
+    -   Single controller
+    -   Using System.Net.Http.HttpClient
+    -   No mappings
+    -   35 seconds do get the 20 best stories
---
+++ src/Model/Story.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Story {
+    public string title { get; set; }
+    public string url { get; set; }
+    public string by { get; set; }
+    public string type { get; set; }
+    public int score { get; set; }
+    public int descendants { get; set; }
+}
